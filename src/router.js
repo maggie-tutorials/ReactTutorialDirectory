5b235023888d7b7ce6914c7f0797dee94c31f3c9
Margherita Serino <m.serino@hotmail.it>
2017-04-08 18:49:29 +0100

    Browser History

--- src/router.js
+++ src/router.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Router, Route, hashHistory } from 'react-router';
+import { Router, Route, browserHistory } from 'react-router';
 
 // Components
 import App from './components/App';
@@ -10,7 +10,7 @@ import Teachers from './components/Teachers';
 
 // Routes
 const routes = (
-  <Router history={hashHistory}>
+  <Router history={browserHistory}>
     <Route component={App}>
       <Route path="/" component={Home} />
       <Route path="about" component={About} />
