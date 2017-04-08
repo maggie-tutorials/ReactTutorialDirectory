d78927542211616cbf14a09a80acc210e820a6f9
Margherita Serino <m.serino@hotmail.it>
2017-04-08 19:14:21 +0100

    Adding IndexRedirect

--- src/router.js
+++ src/router.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Router, Route, browserHistory } from 'react-router';
+import { Router, Route, browserHistory, IndexRedirect } from 'react-router';
 
 // Components
 import App from './components/App';
@@ -8,13 +8,23 @@ import About from './components/About';
 import Courses from './components/Courses';
 import Teachers from './components/Teachers';
 
+import CSS from './components/courses/CSS';
+import HTML from './components/courses/HTML';
+import JavaScript from './components/courses/JavaScript';
+
 // Routes
 const routes = (
   <Router history={browserHistory}>
     <Route component={App}>
       <Route path="/" component={Home} />
       <Route path="about" component={About} />
-      <Route path="courses" component={Courses} />
+
+      <Route path="courses" component={Courses}>
+        <IndexRedirect to="html" />
+        <Route path="css" component={CSS} />
+        <Route path="html" component={HTML} />
+        <Route path="javascript" component={JavaScript} />
+      </Route>
       <Route path="teachers" component={Teachers} />
     </Route>
   </Router>
