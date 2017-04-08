82e606537c71ed6054f5f6d82d4fd70cac753acb
Margherita Serino <m.serino@hotmail.it>
2017-04-08 19:35:54 +0100

    Refactoring CourseContainer

--- src/components/courses/CourseContainer.js
+++ src/components/courses/CourseContainer.js
@@ -1,25 +1,24 @@
 import React, { Component } from 'react';
 
 import Course from './Course';
-import CourseList from '../../data/courses';
 
-class CSS extends Component {
+class CourseContainer extends Component {
   render() {
-    let courseList = CourseList.CSS;
+    let courseList = this.props.route.data;
     let courses = courseList.map((course) => {
       return <Course title={course.title}
                      desc={course.description}
                      img={course.img_src}
                      key={course.id} />
-    }); 
+    });
     return (
       <div>
         <ul>
-          {courses}    
+          {courses}
         </ul>
       </div>
     );
   }
 }
 
-export default CSS;
+export default CourseContainer;
